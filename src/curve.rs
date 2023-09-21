b1a00405844bd4d0a7cc301742b5c47d83836fa7
Mario Cao <mariocao@gmail.com>
2023-09-21 12:13:33 +0200

    feat: add RFC6979 nonce generation function

--- src/curve.rs
+++ src/curve.rs
@@ -171,6 +171,18 @@ where
 
         Ok(primitive.into())
     }
+
+    // ECVRF_nonce_generation_RFC6979(SK, h_string)
+    fn generate_nonce(&self, secret_key: &[u8], digest_msg: &[u8]) -> Result<Scalar<Self::Curve>> {
+        let k = rfc6979::generate_k::<Self::Hasher, <Self::Curve as Curve>::FieldBytesSize>(
+            secret_key.into(),
+            &FieldBytesEncoding::encode_field_bytes(&<Self::Curve as Curve>::ORDER),
+            digest_msg.into(),
+            &[],
+        );
+
+        self.scalar_from_bytes(&k)
+    }
 }
 
 struct P256Sha256;
@@ -199,18 +211,17 @@ impl EcVrf for Secp256k1Sha256 {
 
 #[cfg(test)]
 mod test {
-    use elliptic_curve::sec1::ToEncodedPoint;
     use hex_literal::hex;
 
     use super::*;
 
     #[test]
     fn p256_sha256_tai_encode_to_curve_1() {
-        let my_curve = P256Sha256;
+        let vrf = P256Sha256;
         let public_key = hex!("0360fed4ba255a9d31c961eb74c6356d68c049b8923b61fa6ce669622e60f29fb6");
-        let alpha = hex!("73616d706c65");
+        let alpha = b"sample";
 
-        let point = my_curve.encode_to_curve_tai(&public_key, &alpha).unwrap();
+        let point = vrf.encode_to_curve_tai(&public_key, alpha).unwrap();
 
         let expected_point = hex!("0272a877532e9ac193aff4401234266f59900a4a9e3fc3cfc6a4b7e467a15d06d4");
         assert_eq!(point.to_encoded_point(true).as_bytes(), expected_point);
@@ -218,11 +229,11 @@ mod test {
 
     #[test]
     fn secp256k1_sha256_tai_encode_to_curve() {
-        let my_curve = Secp256k1Sha256;
+        let vrf = Secp256k1Sha256;
         let public_key = hex!("032c8c31fc9f990c6b55e3865a184a4ce50e09481f2eaeb3e60ec1cea13a6ae645");
-        let alpha = hex!("73616d706c65");
+        let alpha = b"sample";
 
-        let point = my_curve.encode_to_curve_tai(&public_key, &alpha).unwrap();
+        let point = vrf.encode_to_curve_tai(&public_key, alpha).unwrap();
 
         let expected_point = hex!("0221ceb1ce22cd34d8b73a619164ed64e917ca31fd454075d02e4bdfa9c5ce0b48");
         assert_eq!(point.to_encoded_point(true).as_bytes(), expected_point);
@@ -231,12 +242,11 @@ mod test {
     /// Same as `p256_sha256_tai_encode_to_curve_1`
     #[test]
     fn test_decode_proof() {
-        let my_curve = P256Sha256;
-
+        let vrf = P256Sha256;
         let pi = hex!(
             "035b5c726e8c0e2c488a107c600578ee75cb702343c153cb1eb8dec77f4b5071b4a53f0a46f018bc2c56e58d383f2305e0975972c26feea0eb122fe7893c15af376b33edf7de17c6ea056d4d82de6bc02f"
         );
-        let (gamma_point, c_scalar, s_scalar) = my_curve.decode_proof(&pi).unwrap();
+        let (gamma_point, c_scalar, s_scalar) = vrf.decode_proof(&pi).unwrap();
 
         // Expected values
         let expected_gamma = p256::AffinePoint::try_from(
@@ -263,6 +273,19 @@ mod test {
         assert!(c_scalar.eq(&expected_c));
         assert!(s_scalar.eq(&expected_s));
     }
+
+    /// Source: RFC 6979 (A.2.5.  ECDSA, 256 Bits (Prime Field))
+    #[test]
+    fn test_nonce_generation() {
+        let vrf = P256Sha256;
+        let secret_key = hex!("C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721");
+        let alpha = b"sample";
+        let k = vrf.generate_nonce(&secret_key, &sha2::Sha256::digest(alpha)).unwrap();
+
+        let expected_k = hex!("A6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60");
+        assert_eq!(k.to_bytes().as_slice(), &expected_k);
+    }
+
 
     // #[test]
     // fn playground() {
